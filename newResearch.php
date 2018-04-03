a9b8655650a51df10f5455287d2b2c1485bca1c4
Nikhil Dhoot <nik.dhoot@gmail.com>
2018-04-03 21:31:16 +0530

    newResearch page initial commit

--- newResearch.php
+++ newResearch.php
@@ -2,7 +2,7 @@
     include './process/connect.php';
     include './process/utils.php';
 
-    $user = get_user();
+    
 ?>
 <!doctype html>
 <html class="no-js" lang="zxx">
@@ -12,7 +12,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>New Startup|SIIC</title>
+    <title>New Project|SIIC</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Favicon Icon Add -->
@@ -62,8 +62,7 @@
                     <div class="row">
                         <div class="col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
                             <div class="main-heading-content text-center">
-                                <h2>Startup Idea<span>.</span></h2>
-                                <p> Be very sure that all your information about you and and your startup will remain confidential and will not be passed to anyone without your prior permission.</p>
+                                <h2>Research Idea<span>.</span></h2>
                             </div>
                         </div>
                     </div>
@@ -77,26 +76,32 @@
         <!-- FORM -->
         <div class="col-md-8 personal-info">
         
-        <form class="form-horizontal" role="form" id="new_startup" enctype="multipart/form-data" action="./process/process_new_startup.php" accept-charset="UTF-8" method="post">
+        <form class="form-horizontal" role="form" id="new_research" enctype="multipart/form-data" action="./process/process_new_research.php" accept-charset="UTF-8" method="post">
           <div class="form-group">
             <label class="col-lg-3 control-label"placeholder="Title" type="text" name="project_title" id="project_title">Title</label>
             <div class="col-lg-8">
-              <input class="form-control" type="text" placeholder="Enter Startup Title">
+              <input class="form-control" type="text" placeholder="Enter Research Title">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="project_requirements" class="col-lg-3 control-label">Report</label>
+            <div class="col-lg-8">
+            <textarea cols="80" rows="10" class="form-control bootsy_text_area" name="project_requirement" id="project_requirements"></textarea>
+            </div>
+          
+
+          <div class="form-group">
+            <label for="project_requirements" class="col-lg-3 control-label">Requirements</label>
+            <div class="col-lg-8">
+            <textarea cols="80" rows="10" class="form-control bootsy_text_area" name="project_requirement" id="project_requirements"></textarea>
             </div>
           </div>
           
           
-
-          <div class="form-group col-lg-9 file_input" >
-          <label for="project_bp" class="control-label">Upload your Project report (4MB File Size Limit)(pdf, ppt, doc, docx)</label><br/>
-                    <input type="file" accept=".pdf, .ppt, .doc, .docx" name="project_bp" id="project_bp"/><br/>
-          </div>
-                  
-          
           <div class="form-group">
             <label class="col-md-3 control-label"></label>
-            <div class="col-md-8" style="padding-left:13rem">
-              <input type="Submit" class="btn btn-primary" value="Save Changes">
+            <div class="col-md-8">
+              <input type="Submit" name="commit" class="btn btn-primary" value="Save Changes">
               <span></span>
               <input type="reset" class="btn btn-default" value="Cancel">
             </div>
@@ -106,17 +111,6 @@
   </div>
 </div>
 <hr>
-
-
-<center><label style="padding-top:10rem"for="startup_datetime">Setup an appointment using calendly! After doing so submit your form.</label><br/>
-                <!-- Calendly inline widget begin -->
-                <center><div class="calendly-inline-widget" data-url="https://calendly.com/siic/score" style="min-width:320px;height:300px;"></div>
-                <script type="text/javascript" src="https://calendly.com/assets/external/widget.js"></script>
-                <!-- Calendly inline widget end -->
-          </div>
- 
-
-
 <script
   src="https://code.jquery.com/jquery-3.3.1.js"
   integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
