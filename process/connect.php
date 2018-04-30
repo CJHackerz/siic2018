a69dc15a79b8d32ca91e87666c34f9891e74b53a
subhadeepkundu <subhadeepkundu98@gmail.com>
2018-04-30 18:45:11 +0530

    minor fix in connect

--- process/connect.php
+++ process/connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "siicn";
+$database = "siic";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
 // Check connection
