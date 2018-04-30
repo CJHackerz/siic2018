dbcbe7a96db76b5aeffc6dc9de4dfedc855585f0
subhadeepkundu <subhadeepkundu98@gmail.com>
2018-04-30 18:47:28 +0530

    process files

---
+++ confirm_prompt.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Freelance|SIIC</title>
+
+    <!-- css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/nivo-lightbox.css" rel="stylesheet" />
+    <link href="css/nivo-lightbox-theme/default/default.css" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link href="css/animations.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet">
+    <link href="color/default.css" rel="stylesheet">
+    <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
+    <link rel="icon" href="./favicon.ico" type="image/x-icon">
+
+</head>
+<style type="text/css">
+  .blogHead{
+    background: url(img/blog-bg.jpg);
+    max-height:304px;
+    width: 100%;
+    text-align: center;
+    color:white;
+  }
+
+
+</style>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+    <div id="wrapper">
+        <div class="overlay"></div>
+<?php
+require "process/connect.php";
+require "includes/nav.php";
+?>
+<div id="page-content-wrapper">
+<p align="center" style="font-size: 50px; padding: 100px;">Your response has been submitted.</p>
+
+
+
+
+</div><!-- Page content wrapper ends -->
+</div><!-- wrapper ends -->
+
+
+
+
+
+    <!-- Core JavaScript Files -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/jquery.scrollTo.js"></script>
+    <script src="js/jquery.appear.js"></script>
+    <script src="js/stellar.js"></script>
+    <script src="js/nivo-lightbox.min.js"></script>
+    <script src="js/sidenav.js"></script>
+
+    <script src="js/custom.js"></script>
+    <script src="js/css3-animate-it.js"></script>
+    <script src="contactform/contactform.js"></script>
+
+</body>
+
+</html>
