21c88ac75275bc5318415224afc57c6425d56bfb
mohit0210 <gupta.mohit0210@gmail.com>
2018-05-20 13:38:14 +0530

    environ vars removed

--- process/connect.php
+++ process/connect.php
@@ -1,12 +1,12 @@
 <?php
-$servername = getenv("SRMIIC_RDS_SERVER");
-$username = getenv("SRMIIC_RDS_USER");
-$password = getenv("SRMIIC_RDS_PASSWORD");
+$servername = "localhost";
+$username = "root";
+$password = "";
 $database = "siic";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+   die("Connection failed: " . $conn->connect_error);
 }
 ?>
