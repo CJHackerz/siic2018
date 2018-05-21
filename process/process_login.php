f231ef6e47a352e444779b6e2e823393d18aa719
unknown <psmailpinaki@gmail.com>
2018-05-21 15:07:19 +0530

    cookie fix

--- process/process_login.php
+++ process/process_login.php
@@ -19,7 +19,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
           $_SESSION["user"]       = $user_email;
           $_SESSION["user_id"]    = $row["uid"];
           $_SESSION["team"]       = "false";
-          setcookie('useremail',$_SESSION['user'],time() + (86400*7), "/");
+          setcookie('useremail',$_SESSION['user'],time() + (86400*7));
           header('Location: ../profile.php');
         }else {
           header('Location: ../signIn.php?error=not_validate');
--- process/process_logout.php
+++ process/process_logout.php
@@ -3,5 +3,6 @@
     unset($_SESSION['user']);
     unset($_SESSION["user_id"]);
     unset($_SESSION['team']);
+    unset($_COOKIE['useremail']);
     header('Location: ../index.php');
 ?>
