bc057a08f52de99cee53b2db10c597fb599cc9ab
mohit0210 <gupta.mohit0210@gmail.com>
2018-05-22 13:32:52 +0530

    forum

---
+++ forum/config.php
@@ -0,0 +1,22 @@
+<?php return array (
+  'debug' => false,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'forum',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'port' => '3306',
+    'strict' => false,
+  ),
+  'url' => 'http://localhost/siic-2018/forum',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
