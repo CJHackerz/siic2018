3e09385d0ba598d79f3118cffef9092304a762fc
mohit0210 <gupta.mohit0210@gmail.com>
2018-06-16 20:34:55 +0530

    env vars set

--- dev/admin/process/connect.php
+++ dev/admin/process/connect.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = getenv("SRMIIC_RDS_SERVER");
+$username = getenv("SRMIIC_RDS_USER");
+$password = getenv("SRMIIC_RDS_PASSWORD");
 $database = "siic";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
