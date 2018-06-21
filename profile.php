6665bc3f7eb6e472badf1abd8c7427d03af39d91
mohit0210 <gupta.mohit0210@gmail.com>
2018-06-22 01:05:30 +0530

    card adjust

--- profile.php
+++ profile.php
@@ -234,7 +234,7 @@ padding-right: 8px;
 <script src="js/vendor/jquery-1.12.0.min.js"></script>
     <script src="js/minix-map.js"></script>
     <script src="js/plugins.js"></script>
-    <script src="js/main.js"></script>
+      <script src="js/main.js"></script>
 
     <input type="hidden" id="uid" value="<?php echo $user;?>">
     <!-- Core JavaScript Files -->
