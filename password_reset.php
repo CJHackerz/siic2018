37b8ba1951615da3edddd779956c0f120b143c52
mohit0210 <gupta.mohit0210@gmail.com>
2018-06-26 02:17:44 +0530

    reset pass form

--- password_reset.php
+++ password_reset.php
@@ -48,7 +48,7 @@
                      ?>
                      <div class="container">
                          <div class="row">
-                             <div class="col-md-6 col-md-offset-3">
+                             <div class="col-md-6">
                                  <div class="panel panel-default devise-bs">
                                      <div class="panel-body">
                                          <h1>Reset Password</h1>
